a9f8a30d84a7cf915e1194c20a8d83aa5dd9ee08
zuckeyandzucchi <au.business.com@gmail.com>
2023-11-25 10:43:54 +0900

    curriculum 08_3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,5 +12,17 @@ class PostController extends Controller
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     } 
     //getPaginateByLimit()はPost.phpで定義したメソッドです。
+
+    /**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+    public function show(Post $post)
+    { 
+        return view('posts.show')->with(['post' => $post]);
+    //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
 }
 ?>
